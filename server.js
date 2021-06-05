08ddc743d0f904f15e46fb16a159e63d8ec22f40
fajrun nadhif <fajrunfifa@gmail.com>
2021-06-05 22:15:21 +0700

    Baru

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 const io = require("socket.io")(server);
+const path = require( 'path' );
+const favicon = require( 'serve-favicon' );
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 // Peer
 
 const { ExpressPeerServer } = require("peer");
@@ -10,11 +14,53 @@ const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
 
+
+
+
+const conn = mysql.createConnection({
+  host: 'localhost', 
+  user: 'fajrun',      
+  password: '123',      
+  database: 'fajrun' 
+}); 
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log('Database is connected successfully !');
+});
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
+//app.use( '/public', express.static( path.join( __dirname, 'public' ) ) );
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/peerjs", peerServer);
 
-app.get("/", (req, rsp) => {
+app.get( '/', ( req, res ) => {
+  res.render("home");
+} );
+
+app.post('/tambah',(req, res) => {
+  let data = {nama: req.body.nama, email: req.body.email, kelamin: req.body.checkbox , pesan: req.body.textarea};
+  let sql = "INSERT INTO webrtc SET ?";
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err;
+   res.redirect('/')
+  });
+});
+
+app.get('/data', (req, res) => {
+  let sql = " SELECT * from webrtc";
+  let query = conn.query(sql, (err, rows) => {
+    if(err) throw err;
+    res.render('data', {
+      title : 'Data buku tamu ',
+      user : rows
+    })
+  })
+})
+
+app.get("/r", (req, rsp) => {
   rsp.redirect(`/${uuidv4()}`);
 });
 
